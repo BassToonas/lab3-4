a8a6bf777bb2b573891133256d0be7e9ac6769a8
SirLancelotJoker <paulius.marma@gmail.com>
2021-04-22 01:44:17 +0300

    Version 0

--- lab3-4/Program.cs
+++ lab3-4/Program.cs
@@ -6,7 +6,43 @@ namespace lab3_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int examGrade, grades=0;
+
+            Console.WriteLine("Iveskite varda\n");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("\nIveskite pavarde\n");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("\nIveskite namu darbu skaiciu\n");
+            int numberOfHomeWork = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberOfHomeWork; i++)
+            {
+                Console.WriteLine("\nIveskite namu darbu pazymi nuo 1-10\n");
+                grades = Convert.ToInt32(Console.ReadLine());
+                if (g > 0 && g <= 10)
+                {
+                    grades += grades;
+                }
+                else
+                {
+                    Console.WriteLine("\nIvestas blogas pazymis\n");
+                    i--;
+                }
+            }
+
+            Console.WriteLine("\nIveskite egzamino ivertinima nuo 1-10\n");
+            g = Convert.ToInt32(Console.ReadLine());
+            if (g > 0 && g <= 10)
+                examGrade = g;
+            else
+            {
+                Console.WriteLine("Ivyko klaida <(O_O<)<(O_O)>(>O_O)>\n");
+                return;
+            }
+
+            double finalResult = (0.3 * (grades / numberOfHomeWork)) + (0.7 * examGrade);
         }
     }
 }
